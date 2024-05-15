79f4d0a169c1b1dc4a6ea6f9b336796127723bbc
Maxime LE GENTIL <maxime.legentil17@gmail.com>
2024-05-15 21:33:06 +0200

    Fixture User + Article

--- Symfony/src/DataFixtures/AppFixtures.php
+++ Symfony/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Blog;
+use App\Entity\User;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -12,6 +15,26 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
+        //$users = [];
+
+        $user = new User();
+        $user->setEmail('maxime.legentil17@gmail.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword('$2y$13$dYgOL5UZOgJlhcbUqO2ceeujKd0pflZvvsobtpIgAp20ygBNFq7Mu'); // Bcrypt -> Mot de passe = admin
+        $user->setFirstname('Maxime');
+        $user->setLastname('LE GENTIL');
+
+        $manager->persist($user);
+        $manager->flush();
+
+        $article = new Blog();
+        $article->setTitle('Ouverture du portfolio');
+        $article->setContent('Le contenu d\'un article ! On peut y insérer des balises HTML.');
+        $article->setAuthor($user);
+        $article->setCreatedAt(new DateTimeImmutable());
+        $article->setImage('https://www.larochelle-tourisme.com/sites/default/files/styles/ogimage/public/medias/images/_32919.jpg?itok=s-Y2oGQ0');
+
+        $manager->persist($article);
         $manager->flush();
     }
 }
